7eb8c604adeab1217ea1117f9b609218d820273c
M. Lutfi Fauzi Sopandi <50583276+Lutfi13@users.noreply.github.com>
2020-06-30 21:53:42 +0700

    Add files via upload

---
+++ twitter_search.py
@@ -0,0 +1,148 @@
+import tweepy
+from tweepy import OAuthHandler
+import json
+import datetime as dt
+import time
+import os
+import sys
+
+
+def load_api():
+
+    consumer_key = ''
+    consumer_secret = ''
+    access_token = ''
+    access_secret = ''
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+
+    return tweepy.API(auth)
+
+    
+def tweet_search(api, query, max_tweets, max_id, since_id, geocode):
+    
+
+    searched_tweets = []
+    while len(searched_tweets) < max_tweets:
+        remaining_tweets = max_tweets - len(searched_tweets)
+        try:
+            new_tweets = api.search(q=query, count=remaining_tweets,
+                                    since_id=str(since_id),
+				                    max_id=str(max_id-1))
+#                                    geocode=geocode)
+            print('found',len(new_tweets),'tweets')
+            if not new_tweets:
+                print('no tweets found')
+                break
+            searched_tweets.extend(new_tweets)
+            max_id = new_tweets[-1].id
+        except tweepy.TweepError:
+            print('exception raised, waiting 15 minutes')
+            print('(until:', dt.datetime.now()+dt.timedelta(minutes=15), ')')
+            time.sleep(15*60)
+            break 
+    return searched_tweets, max_id
+
+
+def get_tweet_id(api, date='', days_ago=9, query='a'):
+
+
+    if date:
+        td = date + dt.timedelta(days=1)
+        tweet_date = '{0}-{1:0>2}-{2:0>2}'.format(td.year, td.month, td.day)
+        tweet = api.search(q=query, count=1, until=tweet_date)
+    else:
+        td = dt.datetime.now() - dt.timedelta(days=days_ago)
+        tweet_date = '{0}-{1:0>2}-{2:0>2}'.format(td.year, td.month, td.day)
+        tweet = api.search(q=query, count=10, until=tweet_date)
+        print('search limit (start/stop):',tweet[0].created_at)
+
+        return tweet[0].id
+
+
+def write_tweets(tweets, filename):
+
+    with open(filename, 'a') as f:
+        for tweet in tweets:
+            json.dump(tweet._json, f)
+            f.write('\n')
+
+
+def main():
+
+
+
+
+    search_phrases = ['Kata_yang_ingin_dicari']
+    time_limit = 1.5                          
+    max_tweets = 100                          
+    min_days_old, max_days_old = 6, 7                                                        
+    IND = '39.8,-95.583068847656,2500km'       
+                                               
+    
+
+    for search_phrase in search_phrases:
+
+        print('Search phrase =', search_phrase)
+
+        name = search_phrase.split()[0]
+        json_file_root = name + '/'  + name
+        os.makedirs(os.path.dirname(json_file_root), exist_ok=True)
+        read_IDs = False
+        
+        if max_days_old - min_days_old == 1:
+            d = dt.datetime.now() - dt.timedelta(days=min_days_old)
+            day = '{0}-{1:0>2}-{2:0>2}'.format(d.year, d.month, d.day)
+        else:
+            d1 = dt.datetime.now() - dt.timedelta(days=max_days_old-1)
+            d2 = dt.datetime.now() - dt.timedelta(days=min_days_old)
+            day = '{0}-{1:0>2}-{2:0>2}_to_{3}-{4:0>2}-{5:0>2}'.format(
+                  d1.year, d1.month, d1.day, d2.year, d2.month, d2.day)
+        json_file = json_file_root + '_' + day + '.json'
+        if os.path.isfile(json_file):
+            print('Appending tweets to file named: ',json_file)
+            read_IDs = True
+        
+        api = load_api()
+        
+        if read_IDs:
+            with open(json_file, 'r') as f:
+                lines = f.readlines()
+                max_id = json.loads(lines[-1])['id']
+                print('Searching from the bottom ID in file')
+        else:
+            if min_days_old == 0:
+                max_id = -1
+            else:
+                max_id = get_tweet_id(api, days_ago=(min_days_old-1))
+
+        since_id = get_tweet_id(api, days_ago=(max_days_old-1))
+        print('max id (starting point) =', max_id)
+        print('since id (ending point) =', since_id)
+        
+
+
+        start = dt.datetime.now()
+        end = start + dt.timedelta(hours=time_limit)
+        count, exitcount = 0, 0
+        while dt.datetime.now() < end:
+            count += 1
+            print('count =',count)
+            tweets, max_id = tweet_search(api, search_phrase, max_tweets,
+                                          max_id=max_id, since_id=since_id,
+                                          geocode=IND)
+            if tweets:
+                write_tweets(tweets, json_file)
+                exitcount = 0
+            else:
+                exitcount += 1
+                if exitcount == 3:
+                    if search_phrase == search_phrases[-1]:
+                        sys.exit('Maximum number of empty tweet strings reached - exiting')
+                    else:
+                        print('Maximum number of empty tweet strings reached - breaking')
+                        break
+
+
+if __name__ == "__main__":
+    main()
